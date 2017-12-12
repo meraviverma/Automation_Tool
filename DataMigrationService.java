179458c6d6c9e8ec61ce35de9585d617832071b7
RAVI VERMA <ravi.verma278@gmail.com>
2017-12-12 15:09:55 +0530

    Update DataMigrationService.java

--- DataMigrationService.java
+++ DataMigrationService.java
@@ -1,8 +1,4 @@
 package com.db.springbootcouchbase.service;
-
-/**
- * Created by RV00451128 on 12/7/2017.
- */
 public interface DataMigrationService {
     void updatefields() throws Exception;
 }
