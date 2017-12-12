ccb62edefdb6b9d55eaa8532bb50c319f3b4b58d
RAVI VERMA <ravi.verma278@gmail.com>
2017-12-12 19:43:56 +0530

    Update DataMigrationDaoImpl.java

--- DataMigrationDaoImpl.java
+++ DataMigrationDaoImpl.java
@@ -16,46 +16,62 @@ import org.springframework.stereotype.Service;
 
 import static org.aspectj.bridge.MessageUtil.info;
 
+/**
+ * Created by Ravi Verma on 12/7/2017.
+ */
 @Slf4j
 @Service
 public class DataMigrationDaoImpl implements DataMigrationDao {
 
-    private final String docType = "facHrr";
     @Autowired
     AbstractCouchbaseConfiguration abstractCouchbaseConfiguration;
 
     @Autowired
     private CouchBaseConfig couchBaseConfig;
-    String bucketName="CIPE_HT";
+
+    private final String docType = "facHrr";
 
     @Override
-    public void updatefieldvalue() throws Exception{
-        Bucket bucket = couchBaseConfig.couchbaseCluster().openBucket(bucketName, "");
+    public void updateFieldValue() throws Exception{
+
+        Bucket bucket=couchBaseConfig.bucket();
+        String bucketName=couchBaseConfig.bucket().name();
+
+        //get docId , facCountryCode,facLocNr from facility document.
         N1qlQuery Query = N1qlQuery.parameterized(" SELECT meta(`" + bucketName + "`).id AS docId,facCountryCode,facLocNr FROM `" + bucketName + "` WHERE docType=$1", JsonArray.from(docType));
         N1qlQueryResult queryResult = bucket.query(Query);
 
         try {
             for (N1qlQueryRow result: queryResult) {
                 JsonObject obj = result.value();
-                String docid=(String) obj.get("docId");
-                String orgdoctype="crdOgzCur-"+(String) obj.get("facCountryCode")+"-"+(String) obj.get("facLocNr");
-                String crdfacdoctype="crdFacCur-"+docid;
+                String docId=(String) obj.get("docId");
 
-                //dataMigrationRepo.findOne(orgdoctype);
-                JsonDocument jsonDocument = bucket.get(orgdoctype);
-                String regionNr=(String)jsonDocument.content().get("regionNr");
-                String disNr=(String)jsonDocument.content().get("districtNr");
-          
-                bucket.mutateIn(docid)
+                //form orgDocType from facility document as crdOgzCur-AD-8061
+                String orgDocType="crdOgzCur-"+(String) obj.get("facCountryCode")+"-"+(String) obj.get("facLocNr");
+                //form crdFacDocType
+                String crdFacDocType="crdFacCur-"+docId;
+
+
+                JsonDocument jsonDocument = bucket.get(orgDocType);
+                String regionNr=(String)jsonDocument.content().get("regionNr");//get regionNr from orgDocType
+                String disNr=(String)jsonDocument.content().get("districtNr");//get districtNr from orgDocType
+
+               /* System.out.println("*******orgdoctype******"+orgdoctype);
+                System.out.println("*****crdfacdoctype********"+crdfacdoctype);
+                System.out.println("******obj*******"+regionNr);
+                System.out.println("******obj*******"+disNr);*/
+
+               //update  facility document
+                bucket.mutateIn(docId)
                         .upsert("ownRegNr",regionNr,true)
                         .upsert("ownDisNr",disNr,true)
                         .execute();
-
-                bucket.mutateIn(crdfacdoctype)
+                
+                //update crd document
+                bucket.mutateIn(crdFacDocType)
                         .upsert("ownRegNr",regionNr,true)
                         .upsert("ownDisNr",disNr,true)
                         .execute();
-               
             }
         } catch(Exception e) {
             info(e.getMessage());
