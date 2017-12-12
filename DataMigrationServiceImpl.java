83fd6c3c271455d627347f58105608c0daa314e0
RAVI VERMA <ravi.verma278@gmail.com>
2017-12-12 15:10:22 +0530

    Update DataMigrationServiceImpl.java

--- DataMigrationServiceImpl.java
+++ DataMigrationServiceImpl.java
@@ -5,9 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-/**
- * Created by RV00451128 on 12/7/2017.
- */
 @Slf4j
 @Service
 public class DataMigrationServiceImpl implements DataMigrationService{
